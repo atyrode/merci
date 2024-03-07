3a85145897bdf2582efc11006377e3f8c5f301ab
atyrode <alex.tyrode@outlook.fr>
2024-03-07 14:13:26 +0100

    implement draft of table using vue good table next #5

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+import VueGoodTablePlugin from 'vue-good-table-next';
+import 'vue-good-table-next/dist/vue-good-table-next.css'
 
+app.use(VueGoodTablePlugin);
+app.use(router)
 app.mount('#app')
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import DynamicTable from '../components/DynamicTable.vue'
 </script>
 
 <template>
   <main>
+    <DynamicTable />
   </main>
 </template>
