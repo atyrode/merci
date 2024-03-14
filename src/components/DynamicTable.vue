3f5da65df8dce18b959811ef9a7b5ae918b73665
atyrode <alex.tyrode@outlook.fr>
2024-03-14 21:02:31 +0100

    change let to const for static declarations #11

--- src/components/DynamicTable.vue
+++ src/components/DynamicTable.vue
@@ -33,8 +33,8 @@ export default {
             return value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")
         },
         calculateAverage(rows: any[], column: { field: any; type: any; }) {
-            let field = column.field;
-            let type = column.type;
+            const field = column.field;
+            const type = column.type;
 
             if (field == "hotelName") {
                 return 'Average';
